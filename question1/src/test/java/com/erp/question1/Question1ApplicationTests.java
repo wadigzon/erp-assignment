a80bb32ea336aa1bfd7ade26e90856145e6378cf
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2021-09-30 23:09:18 -0700

    added spring boot starting app

---
+++ question1/src/test/java/com/erp/question1/Question1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.question1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
